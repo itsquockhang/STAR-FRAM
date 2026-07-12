3db2ab45ca1e97ea73495b4e3d6dcdd94f692efd
Quoc-Khang Tran <quockhang@Quoc-Khangs-MacBook-Pro.local>
2026-07-12 09:05:14 +0700

    docs: add usage curl example to serve script

--- scripts/serve_translategemma4b.sh
+++ scripts/serve_translategemma4b.sh
@@ -1,5 +1,17 @@
 # docker pull vllm/vllm-openai:v0.14.1-cu130
 
+# curl -X POST http://localhost:8128/v1/chat/completions \
+#   -H "Content-Type: application/json" \
+#   -d '{
+#     "model": "translategemma-4b-it",
+#     "messages": [
+#         {
+#           "role": "user",
+#           "content": "<<<source>>>en<<<target>>>zh<<<text>>>We distribute two models for language identification, which can recognize 176 languages."
+#         }
+#       ]
+#     }'
+
 docker run -itd --name google-translategemma-4b-it \
     --ipc=host \
     --network host \
